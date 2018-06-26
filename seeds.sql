b22d289b6a1ae6bcc9f09d5da31553c6fbd9677b
Claudia <riosnclaudia@gmail.com>
2018-06-26 13:40:29 -0400

    Create seeds.sql

---
+++ seeds.sql
@@ -0,0 +1,11 @@
+INSERT INTO burgers (burger_name, finnito)
+VALUES ("Sushi Burger", false);
+
+INSERT INTO burgers (burger_name, finnito )
+VALUES ("Avocado Burger", false);
+
+INSERT INTO burgers (burger_name, finnito)
+VALUES ("Spicy salmon Burger", false);
+
+INSERT INTO burgers (burger_name, finnito)
+VALUES ("Crab Burger", true);
